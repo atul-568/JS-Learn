b9a79a2df7f5d4cd5b6c7afcecf6045bcd616464
atul-568 <92644735+atul-568@users.noreply.github.com>
2024-12-04 11:08:40 +0000

    learning datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,12 @@
+// MDN and ic3 ECMA for Documentation
+// "use strict"; treat all js code as newer version
+// alert(3+3) we are using node js not browser
+
+// primitive datatypes
+// Number, bigint, string, boolean, null(standalone value), undefined, symbol(unique)
+
+// object
+// NULL ka type => object hai
+// undefined ka type => undefined hai
+// typeof to check types.
+
