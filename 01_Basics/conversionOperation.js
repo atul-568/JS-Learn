ddae3ea340092f83dcd6ac805f43afbbedc50048
atul-568 <92644735+atul-568@users.noreply.github.com>
2024-12-05 07:02:10 +0000

    ConversionOperation learning

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,14 @@
+// typeof to get the type of any field
+// Number(var), bigint(var), string(var), boolean(var), null(standalone value), undefined, symbol(unique) for conversion from one datatype to other
+
+// Operations
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");  
+console.log(+true);
+console.log(+""); //0
+
+  
+// to know more about these go to the Abstract Operation=>type conversion in tc39.es(ECMA script) 
+// Go to the documentation to know more
