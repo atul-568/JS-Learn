571dc236588d47a32c2fe6790da9b0a99e4608a7
atul-568 <92644735+atul-568@users.noreply.github.com>
2024-12-05 07:24:54 +0000

    some additional infromation

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -12,3 +12,18 @@ console.log(+""); //0
   
 // to know more about these go to the Abstract Operation=>type conversion in tc39.es(ECMA script) 
 // Go to the documentation to know more
+
+console.log("2">1);
+console.log("02">1);
+console.log(null>0);
+console.log(null==0);  
+console.log(null>=0);
+
+//  the reason is that an equality check == and comparison ><>==<= work differntly comparison convert null to a number, treating it as 0. that's why (3) null>=0 is true and(1) null>0 is false.
+
+console.log(undefined>0);
+console.log(undefined==0);  
+console.log(undefined>=0); // always false
+
+// === strict check
+
