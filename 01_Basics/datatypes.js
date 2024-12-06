c0489447bb7b4d687ed9d0dc7e51587b1f131fee
atul-568 <92644735+atul-568@users.noreply.github.com>
2024-12-06 11:02:07 +0000

    Add on learning

--- 01_Basics/datatypes.js
+++ 01_Basics/datatypes.js
@@ -5,21 +5,21 @@
 // primitive datatypes
 // Number, bigint, string, boolean, null(standalone value), undefined, symbol(unique)
 
-const id = Symbol('123')
-const A_id = Symbol('123')
-console.log((id===A_id));
+// const id = Symbol('123')
+// const A_id = Symbol('123')
+// console.log((id===A_id));
 
 // Non-primitive(Referenced) datatypes
 // object, array, function
 
-let a = [1,2,3,4]
+// let a = [1,2,3,4]
 
-let b ={
-    name: war,
-    id:1
-}
+// let b ={
+//     name: war,
+//     id:1
+// }
 
-const myFunction=function(){}
+// const myFunction=function(){}
 
 
 // Stack(primitive hamesha hota hai) ,heap(Non-primitive(Referenced))
@@ -30,3 +30,12 @@ const myFunction=function(){}
 // undefined ka type => undefined hai
 // typeof to check types.
 
+const name = "Atul"
+const password = "12345"
+console.log(`my name is ${name} and the password is ${password}`);
+// other way to define string :-        const gameName = newString('Atul')
+
+const Gname = new String("atul")
+console.log(Gname.__proto__);
+console.log(Gname.toLowerCase());
+//  for string method go check from the MDN documentation
